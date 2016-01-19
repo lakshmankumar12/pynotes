3154c8b346dbff9fe7a7538d05ff0f48b152a0e5
lnara002 <lakshman_kumar.narayanan@alcatel-lucent.com>
2016-01-19 13:37:40 -0800

    make combinate more elegant

--- combinate.py
+++ combinate.py
@@ -7,13 +7,10 @@ def choose(available,so_far,to_pick,verbose=False):
     if verbose:
       print(so_far)
     return 1
-  if to_pick == 1:
-    return sum(choose([],so_far+[i],0,verbose) for i in available)
-  if to_pick >= len(available):
-    if verbose:
-      print(so_far+available)
-    return 1
-  return choose(available[1:],so_far+[available[0]],to_pick-1,verbose) + choose(available[1:],so_far,to_pick,verbose)
+  s = 0
+  if to_pick < len(available):
+    s = choose(available[1:],so_far,to_pick,verbose)
+  return s + choose(available[1:],so_far+[available[0]],to_pick-1,verbose)
 
 if __name__ == '__main__':
   n=int(sys.argv[1])
